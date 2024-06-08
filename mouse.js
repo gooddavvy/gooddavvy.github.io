790fb38fe35ea85a604b7df1d1fe7ab24ca9da7e
David AKIN <110428134+gooddavvy@users.noreply.github.com>
2024-06-08 08:26:11 -0500

    custom cursor

--- mouse.js
+++ mouse.js
@@ -1,20 +1,14 @@
 // Create a new div element for the mouse cursor
 var cursor = document.createElement('div');
-cursor.style.width = '10px !important';
-cursor.style.height = '10px !important';
-cursor.style.borderRadius = '50% !important';
-cursor.style.backgroundColor = 'white !important';
-cursor.style.position = 'fixed !important';
-cursor.style.zIndex = '9999 !important';
-cursor.style.pointerEvents = 'none !important'; // Ensure the cursor does not interfere with mouse events
+cursor.className = 'custom-cursor';
 
 // Append the cursor element to the body
 document.body.appendChild(cursor);
 
 // Function to update the cursor position based on mouse movements
 function updateCursorPosition(event) {
-    cursor.style.left = (event.clientX - cursor.offsetWidth / 2) + 'px !important';
-    cursor.style.top = (event.clientY - cursor.offsetHeight / 2) + 'px !important';
+    cursor.style.left = event.clientX + 'px';
+    cursor.style.top = event.clientY + 'px';
 }
 
 // Add event listener for mouse movements
